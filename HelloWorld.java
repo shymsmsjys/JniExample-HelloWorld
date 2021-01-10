bd018dd66790831d2cf6cc8eeddd3a9d4c366b5b
shcha <csh112@nate.com>
2021-01-10 11:13:06 +0900

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,33 @@
+package kr.or.aesop;
+
+public class HelloWorld {
+	static {
+		System.load("helloworld.so");
+	}
+	public static void main(String args[]) {
+		HelloWorld h = new HelloWorld();
+
+		String a = "AA\n";
+		int[] b = {1,2,3};
+		char[] c = {'a', 'b', 'c'};
+		System.out.println(a);
+		h.show1();
+		h.show2(9);
+		h.show3(b);
+		h.show4('d');
+		h.show5(c);
+		h.show6(a);
+		h.show6();
+	}
+	public void func() {
+		System.out.println("func");
+	}
+	
+	public native void show1();
+	public native void show2(int i);
+	public native void show3(int i[]);
+	public native void show4(char c);
+	public native void show5(char c[]);
+	public native void show6(String s);
+	public native void show6();
+}
---
+++ helloworld.c
@@ -0,0 +1,57 @@
+#include "kr_or_aesop_HelloWorld.h"
+#include <stdio.h>
+#include <sudlib.h>
+#include <string.h>
+
+JNIEXPORT void JNICALL Java_kr_or_aesop_HelloWord_show1 (JNIEnv *a, jobject b) {
+	printf("show1\n");
+}
+
+JNIEXPORT void JNICALL Java_kr_or_aesop_HelloWord_show2 (JNIEnv *a, jobject b, jint c) {
+	printf("show2\n");
+
+}
+
+JNIEXPORT void JNICALL Java_kr_or_aesop_HelloWord_show3 (JNIEnv *a, jobject b, jintArray c) {
+	jint *A = NULL;
+	A = (*a)->GetIntArrayElements(a,c,NULL);
+	if (A==NULL) {
+		printf("error1");
+	}
+	printf("show3 : %d %d %d \n", A[0], A[1], A[2]);
+}
+
+JNIEXPORT void JNICALL Java_kr_or_aesop_HelloWord_show4 (JNIEnv *a, jobject b, jchar c) {
+	printf("show4 : %c \n",c);
+}
+
+JNIEXPORT void JNICALL Java_kr_or_aesop_HelloWord_show5 (JNIEnv *a, jobject b, jintArray c) {
+        jchar *A = NULL;
+        A = (*a)->GetCharArrayElements(a,c,NULL);
+        if (A==NULL) {
+                printf("error1");
+        }
+        printf("show5 : %c %c %c \n", A[0], A[1], A[2]);
+}
+
+JNIEXPORT void JNICALL Java_kr_or_aesop_HelloWord_show6__LJava_lang_String_2(JNIEnv *a, jobject b, jstring c) {
+	char strbuf[128];
+	const char *sz = (*a)->GetStringUTFChars(a, c, 0);
+	strcpy(strbuf, sz);
+	printf("show6 : %s", strbuf);
+	(*a)->ReleaseStringUTFChars(a, c, sz);
+}
+
+JNIEXPORT void JNICALL Java_kr_or_aesop_HelloWord_show6__ (JNIEnv *a, jobject b) {
+	jclass cls = (*a)->GetObjectClass(a,b);
+	
+	jmethodID funcM = (*a)->GetMethodID(a,cls,"func", "()V");
+	if (funcM == 0) {
+		printf(Method func ?? \n");
+	} else {
+		printf("Method fuck ok ..........\n");
+		(*a)->CallVoidMethod(a,b, funcM);
+	}
+}
+	if (func
+
